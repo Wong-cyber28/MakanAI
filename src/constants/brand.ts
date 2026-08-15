c12b43a25f11f39e4d6e732d45b95f02d2bac744
WYB <wyb8467@gmail.com>
2026-08-15 10:00:00 +0900

    chore: init Expo & React Native project with base configs

---
+++ src/constants/brand.ts
@@ -0,0 +1,9 @@
+export const PANDAN = '#4C8566';
+export const WARM_BEIGE = '#F9F6F0';
+export const MUTED_ICON = '#B0AAA2';
+
+export const MACRO_TARGETS = {
+  protein: 100,
+  carbs: 250,
+  fat: 70,
+} as const;
